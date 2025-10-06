7e1e7befc8425afb7106a77443082f54654ec3f2
nick.gha <alessio.ferrari@marconirovereto.it>
2025-10-06 11:56:47 +0200

    Feat: implement methods

--- astronomia/src/Sistema.java
+++ astronomia/src/Sistema.java
@@ -10,14 +10,61 @@ public class Sistema {
     }
 
     public void aggiungi(CorpoCeleste cc) {
-
+        corpiCelesti.add(cc); // aggiungo il corpo celeste
     }
 
     public int contaStelle() {
+        int c = 0;
+        for (CorpoCeleste corpoCeleste : corpiCelesti) {
+            if (corpoCeleste instanceof Stella) {
+                c++;
+            }
+        }
+        return c;
+    }
+
+    public boolean pianetiConsecutivi() {
+
+        int x = 0;
+
+        // Scorro tutto l array
+        for (int i = 0; i < corpiCelesti.size(); i++) {
+
+            // Se corpicelesti.get(i) è un piaeta, controllo quelli successivi
+            if (corpiCelesti.get(i) instanceof Pianeta) {
+
+                for (x++; x < corpiCelesti.size(); x++) {
+                    if (corpiCelesti.get(x) instanceof Pianeta) {
+
+                    } else {
+
+                        return false;
+
+                    }
+
+                }
+                x++;
+
+            }
+
+        }
+
+        return true;
 
     }
 
-    
+    // Rimozione di un pianeta inserendo il suo nome
+    public int rimuoviPianeti(String s) {
+        int x = 0;
+        for (int i = 0; i < corpiCelesti.size(); i++) {
+            if (corpiCelesti.get(i).getNome().contains(s)) {
+                corpiCelesti.remove(i);
+                x++;
+            }
+        }
+        return x;
+    }
 }
+
 
 
