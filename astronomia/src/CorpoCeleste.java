bbf7119343337d4d064fd6caf70a287d270d5e2b
nick.gha <alessio.ferrari@marconirovereto.it>
2025-10-06 11:00:13 +0200

    Feat: add attributes + methods to CorpoCeleste

--- astronomia/src/CorpoCeleste.java
+++ astronomia/src/CorpoCeleste.java
@@ -1,3 +1,29 @@
-public class CorpoCeleste {
+public abstract class CorpoCeleste {
     
+    // protected 
+    protected String nome;
+    protected double distanzaStella, massa, raggio;
+
+    // costante gravitazionale
+    public double G = 6.67408e-11;
+
+    public CorpoCeleste(String nome, double distanzaStella, double massa, double raggio) { // costruttore vuoto da usare con super nelle sottoclassi
+        this.nome = nome;
+        this.distanzaStella = distanzaStella;
+        this.massa = massa;
+        this.raggio = raggio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getDistanzaStella() {
+        return distanzaStella;
+    }
+
+    // impossibile da modificare
+    public final double getGravita() {
+        return G * this.massa / Math.pow(raggio, 2);
+    }
 }
